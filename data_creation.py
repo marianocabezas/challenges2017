221a8e45e348ce6f66563469567d866284171b7a
Mariano Cabezas <mcabezas@eia.udg.edu>
2017-06-30 17:09:17 +0200

    - And WHO the FUCK gives random numbers as labels instead or ordered integers?

--- data_creation.py
+++ data_creation.py
@@ -117,6 +117,7 @@ def load_patch_batch_generator_train(
         if split:
             if iseg:
                 vals = np.unique(y.flatten())
+                print(vals)
                 y_cat = np.sum(
                     map(lambda (lab, val): np.array(y == val, dtype=np.uint8)*lab, enumerate(vals)), axis=0
                 )
