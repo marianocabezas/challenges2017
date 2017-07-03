12a61b195e4a8b4271a38123d07d55975b1e5ee0
Mariano Cabezas <mcabezas@eia.udg.edu>
2017-07-03 17:57:30 +0200

    - Lame shit

--- train_test_iseg.py
+++ train_test_iseg.py
@@ -193,7 +193,8 @@ def main():
                     t2 = Dropout(0.5)(t2)
                     t1 = Dropout(0.5)(t1)
                     brain_patch_f = Dense(108, activation='softmax', name='brain')(concatenate([t2, t1], axis=1))
-                    brain_patch = Reshape((4, 3, 3, 3))(brain_patch_f)
+                    brain_patch_f = Dropout(0.5)(brain_patch_f)
+                    brain_patch = brain_patch_f
                 else:
                     brain_patch = []
                     brain_patch_f = []
