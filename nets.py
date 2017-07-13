5ee098d720e4b7cba1709108b9c01d59c7017abd
Mariano Cabezas <mcabezas@eia.udg.edu>
2017-07-13 12:32:19 +0200

    - iSeg stuff - BRATS stuff

---
+++ nets.py
@@ -0,0 +1,133 @@
+from keras import backend as K
+from keras.layers import Dense, Conv3D, Dropout, Flatten, Input, concatenate, Reshape, Lambda
+from keras.layers import BatchNormalization, LSTM, Permute, Activation, PReLU
+from keras.models import Model
+
+
+def compile_network(inputs, outputs, weights):
+    net = Model(inputs=inputs, outputs=outputs)
+
+    net.compile(
+        optimizer='adadelta',
+        loss='categorical_crossentropy',
+        loss_weights=weights,
+        metrics=['accuracy']
+    )
+
+    return net
+
+
+def get_convolutional_block(input_l, filters_list, kernel_size_list, activation=PReLU, drop=0.5):
+    for filters, kernel_size in zip(filters_list, kernel_size_list):
+        input_l = Conv3D(filters, kernel_size=kernel_size, data_format='channels_first')(input_l)
+        input_l = BatchNormalization(axis=1)(input_l)
+        input_l = activation()(input_l)
+        input_l = Dropout(drop)(input_l)
+
+    return input_l
+
+
+def get_tissue_binary_stuff(input_l):
+    csf = Dense(2)(input_l)
+    gm = Dense(2)(input_l)
+    wm = Dense(2)(input_l)
+    csf_out = Activation('softmax', name='csf')(csf)
+    gm_out = Activation('softmax', name='gm')(gm)
+    wm_out = Activation('softmax', name='wm')(wm)
+
+    return csf, gm, wm, csf_out, gm_out, wm_out
+
+
+def get_network_1(input_shape, filters_list, kernel_size_list, dense_size):
+    merged_inputs = Input(shape=input_shape, name='merged_inputs')
+    # Input splitting
+    input_shape = K.int_shape(merged_inputs)
+    t1 = Lambda(lambda l: K.expand_dims(l[:, 0, :, :, :], axis=1), output_shape=(1,) + input_shape[2:])(merged_inputs)
+    t2 = Lambda(lambda l: K.expand_dims(l[:, 1, :, :, :], axis=1), output_shape=(1,) + input_shape[2:])(merged_inputs)
+
+    # Convolutional part
+    t2 = get_convolutional_block(t2, filters_list, kernel_size_list)
+    t1 = get_convolutional_block(t1, filters_list, kernel_size_list)
+
+    # Tissue binary stuff
+    t2_f = Flatten()(t2)
+    t1_f = Flatten()(t1)
+    t2_f = Dense(dense_size, activation='relu')(t2_f)
+    t2_f = Dropout(0.5)(t2_f)
+    t1_f = Dense(dense_size, activation='relu')(t1_f)
+    t1_f = Dropout(0.5)(t1_f)
+    merged = concatenate([t2_f, t1_f])
+    csf, gm, wm, csf_out, gm_out, wm_out = get_tissue_binary_stuff(merged)
+
+    # Final labeling
+    merged = concatenate([t2_f, t1_f, PReLU()(csf), PReLU()(gm), PReLU()(wm)])
+    merged = Dropout(0.5)(merged)
+    brain = Dense(4, name='brain', activation='softmax')(merged)
+
+    # Weights and outputs
+    weights = [0.2,     0.5,    0.5,    1.0]
+    outputs = [csf_out, gm_out, wm_out, brain]
+
+    return compile_network(merged_inputs, outputs, weights)
+
+
+def get_network_2(input_shape, filters_list, kernel_size_list, dense_size):
+    merged_inputs = Input(shape=input_shape, name='merged_inputs')
+    # Convolutional stuff
+    merged = get_convolutional_block(merged_inputs, filters_list, kernel_size_list)
+
+    # Tissue binary stuff
+    merged_f = Flatten()(merged)
+    merged_f = Dense(dense_size, activation='relu')(merged_f)
+    merged_f = Dropout(0.5)(merged_f)
+    csf, gm, wm, csf_out, gm_out, wm_out = get_tissue_binary_stuff(merged_f)
+
+    # Final labeling stuff
+    merged = concatenate([PReLU()(csf), PReLU()(gm), PReLU()(wm), merged_f])
+    merged = Dropout(0.5)(merged)
+    brain = Dense(4, activation='softmax', name='brain')(merged)
+
+    # Weights and outputs
+    weights = [0.2,     0.5,    0.5,    1.0]
+    outputs = [csf_out, gm_out, wm_out, brain]
+
+    return compile_network(merged_inputs, outputs, weights)
+
+
+def get_network_3(input_shape, filters_list, kernel_size_list, dense_size):
+    merged_inputs = Input(shape=input_shape, name='merged_inputs')
+    # Convolutional part
+    merged = get_convolutional_block(merged_inputs, filters_list, kernel_size_list)
+
+    # LSTM stuff
+    patch_center = Reshape((filters_list[-1], -1))(merged)
+    patch_center = Dense(4, name='pre_rf')(Permute((2, 1))(patch_center))
+    rf = LSTM(4, implementation=1)(patch_center)
+    rf_out = Activation('softmax', name='rf_out')(rf)
+    rf = Dropout(0.5)(PReLU(name='rf')(rf))
+
+    # Tissue binary stuff
+    merged_f = Flatten()(merged)
+    merged_f = Dense(dense_size, activation='relu')(merged_f)
+    merged_f = Dropout(0.5)(merged_f)
+    csf, gm, wm, csf_out, gm_out, wm_out = get_tissue_binary_stuff(merged_f)
+
+    # Brain labeling
+    csf = Dropout(0.5)(PReLU()(csf))
+    gm = Dropout(0.5)(PReLU()(gm))
+    wm = Dropout(0.5)(PReLU()(wm))
+    merged = concatenate([csf, gm, wm, merged_f])
+    merged = Dropout(0.5)(merged)
+    brain = Dense(4)(merged)
+    br_out = Activation('softmax', name='brain_out')(brain)
+    brain = Dropout(0.5)(PReLU(name='brain')(brain))
+
+    # Final labeling
+    final_layers = concatenate([csf, gm, wm, brain, rf])
+    final = Dense(4, name='merge', activation='softmax')(final_layers)
+
+    # Weights and outputs
+    weights = [0.2,     0.5,    0.5,    0.8,    0.8,    1.0]
+    outputs = [csf_out, gm_out, wm_out, br_out, rf_out, final]
+
+    return compile_network(merged_inputs, outputs, weights)
--- train_test_iseg.py
+++ train_test_iseg.py
@@ -3,11 +3,7 @@ import argparse
 import os
 from time import strftime
 import numpy as np
-import keras
-from keras import backend as K
-from keras.models import Sequential, Model
-from keras.layers import Dense, Conv3D, Dropout, Flatten, Input, concatenate, Reshape, Lambda
-from keras.layers import BatchNormalization, LSTM, Permute, Activation, PReLU
+from keras.models import load_model
 from nibabel import load as load_nii
 from nibabel import save as save_nii
 from utils import color_codes, nfold_cross_validation
@@ -16,6 +12,7 @@ from data_creation import load_patch_batch_train, get_cnn_centers
 from data_creation import load_patch_batch_generator_test
 from data_manipulation.generate_features import get_mask_voxels
 from data_manipulation.metrics import dsc_seg
+from nets import get_network_1, get_network_2, get_network_3
 
 
 def parse_inputs():
@@ -58,123 +55,7 @@ def get_names_from_path(options):
     return image_names, label_names
 
 
-def get_convolutional_block(input_l, filters_list, kernel_size_list, activation=PReLU, drop=0.5):
-    for filters, kernel_size in zip(filters_list, kernel_size_list):
-        input_l = Conv3D(filters, kernel_size=kernel_size, data_format='channels_first')(input_l)
-        input_l = BatchNormalization(axis=1)(input_l)
-        input_l = activation()(input_l)
-        input_l = Dropout(drop)(input_l)
-
-    return input_l
-
-
-def get_tissue_binary_stuff(input_l):
-    csf = Dense(2)(input_l)
-    gm = Dense(2)(input_l)
-    wm = Dense(2)(input_l)
-    csf_out = Activation('softmax', name='csf')(csf)
-    gm_out = Activation('softmax', name='gm')(gm)
-    wm_out = Activation('softmax', name='wm')(wm)
-
-    return csf, gm, wm, csf_out, gm_out, wm_out
-
-
-def get_network_1(merged_inputs, filters_list, kernel_size_list, dense_size):
-    # Input splitting
-    input_shape = K.int_shape(merged_inputs)
-    t1 = Lambda(lambda l: K.expand_dims(l[:, 0, :, :, :], axis=1), output_shape=(1,) + input_shape[2:])(merged_inputs)
-    t2 = Lambda(lambda l: K.expand_dims(l[:, 1, :, :, :], axis=1), output_shape=(1,) + input_shape[2:])(merged_inputs)
-
-    # Convolutional part
-    t2 = get_convolutional_block(t2, filters_list, kernel_size_list)
-    t1 = get_convolutional_block(t1, filters_list, kernel_size_list)
-
-    # Tissue binary stuff
-    t2_f = Flatten()(t2)
-    t1_f = Flatten()(t1)
-    t2_f = Dense(dense_size, activation='relu')(t2_f)
-    t2_f = Dropout(0.5)(t2_f)
-    t1_f = Dense(dense_size, activation='relu')(t1_f)
-    t1_f = Dropout(0.5)(t1_f)
-    merged = concatenate([t2_f, t1_f])
-    csf, gm, wm, csf_out, gm_out, wm_out = get_tissue_binary_stuff(merged)
-
-    # Final labeling
-    merged = concatenate([t2_f, t1_f, PReLU()(csf), PReLU()(gm), PReLU()(wm)])
-    merged = Dropout(0.5)(merged)
-    brain = Dense(4, name='brain', activation='softmax')(merged)
-
-    # Weights and outputs
-    weights = [0.2,     0.5,    0.5,    1.0]
-    outputs = [csf_out, gm_out, wm_out, brain]
-
-    return weights, outputs
-
-
-def get_network_2(merged_inputs, filters_list, kernel_size_list, dense_size):
-    # Convolutional stuff
-    merged = get_convolutional_block(merged_inputs, filters_list, kernel_size_list)
-
-    # Tissue binary stuff
-    merged_f = Flatten()(merged)
-    merged_f = Dense(dense_size, activation='relu')(merged_f)
-    merged_f = Dropout(0.5)(merged_f)
-    csf, gm, wm, csf_out, gm_out, wm_out = get_tissue_binary_stuff(merged_f)
-
-    # Final labeling stuff
-    merged = concatenate([PReLU()(csf), PReLU()(gm), PReLU()(wm), merged_f])
-    merged = Dropout(0.5)(merged)
-    brain = Dense(4, activation='softmax', name='brain')(merged)
-
-    # Weights and outputs
-    weights = [0.2,     0.5,    0.5,    1.0]
-    outputs = [csf_out, gm_out, wm_out, brain]
-
-    return weights, outputs
-
-
-def get_network_3(merged_inputs, filters_list, kernel_size_list, dense_size):
-    # Convolutional part
-    merged = get_convolutional_block(merged_inputs, filters_list, kernel_size_list)
-
-    # LSTM stuff
-    patch_center = Reshape((filters_list[-1], -1))(merged)
-    patch_center = Dense(4, name='pre_rf')(Permute((2, 1))(patch_center))
-    rf = LSTM(4, implementation=1)(patch_center)
-    rf_out = Activation('softmax', name='rf_out')(rf)
-    rf = Dropout(0.5)(PReLU(name='rf')(rf))
-
-    # Tissue binary stuff
-    merged_f = Flatten()(merged)
-    merged_f = Dense(dense_size, activation='relu')(merged_f)
-    merged_f = Dropout(0.5)(merged_f)
-    csf, gm, wm, csf_out, gm_out, wm_out = get_tissue_binary_stuff(merged_f)
-
-    # Brain labeling
-    csf = Dropout(0.5)(PReLU()(csf))
-    gm = Dropout(0.5)(PReLU()(gm))
-    wm = Dropout(0.5)(PReLU()(wm))
-    merged = concatenate([csf, gm, wm, merged_f])
-    merged = Dropout(0.5)(merged)
-    brain = Dense(4)(merged)
-    br_out = Activation('softmax', name='brain_out')(brain)
-    brain = Dropout(0.5)(PReLU(name='brain')(brain))
-
-    # Final labeling
-    final_layers = concatenate([csf, gm, wm, brain, rf])
-    final = Dense(4, name='merge', activation='softmax')(final_layers)
-
-    # Weights and outputs
-    weights = [0.2,     0.5,    0.5,    0.8,    0.8,    1.0]
-    outputs = [csf_out, gm_out, wm_out, br_out, rf_out, final]
-
-    return weights, outputs
-
-
-def main():
-    options = parse_inputs()
-    c = color_codes()
-
+def train_net(fold_n, train_data, train_labels, val_data, val_labels, options):
     # Prepare the net architecture parameters
     dfactor = options['dfactor']
     # Prepare the net hyperparameters
@@ -201,6 +82,155 @@ def main():
     params_s = (exp_s, dfactor, patch_width, conv_s, filters_s, dense_size, epochs)
     sufix = '%s.D%d.p%d.c%s.n%s.d%d.e%d.' % params_s
 
+    net_name = os.path.join(path, 'iseg2017.fold%d' % fold_n + sufix + 'mdl')
+
+    c = color_codes()
+    try:
+        net = load_model(net_name)
+    except IOError:
+        # NET definition using Keras
+        train_centers = get_cnn_centers(train_data[:, 0], train_labels)
+        val_centers = get_cnn_centers(val_data[:, 0], val_labels)
+        train_samples = len(train_centers) / dfactor
+        val_samples = len(val_centers) / dfactor
+        print(c['c'] + '[' + strftime("%H:%M:%S") + ']    ' + c['g'] + 'Creating and compiling the model ' +
+              c['b'] + '(%d samples)' % train_samples + c['nc'])
+        train_steps_per_epoch = -(-train_samples / batch_size)
+        val_steps_per_epoch = -(-val_samples / batch_size)
+        input_shape = (2,) + patch_size
+        # This architecture is based on the functional Keras API to introduce 3 output paths:
+        # - Whole tumor segmentation
+        # - Core segmentation (including whole tumor)
+        # - Whole segmentation (tumor, core and enhancing parts)
+        # The idea is to let the network work on the three parts to improve the multiclass segmentation.
+        network_func = [get_network_1, get_network_2, get_network_3]
+        net = network_func[experimental](
+            input_shape,
+            filters_list,
+            kernel_size_list,
+            dense_size
+        )
+
+        print(c['c'] + '[' + strftime("%H:%M:%S") + ']    ' +
+              c['g'] + 'Training the model with a generator for ' +
+              c['b'] + '(%d parameters)' % net.count_params() + c['nc'])
+        print(net.summary())
+        net.fit_generator(
+            generator=load_patch_batch_train(
+                image_names=train_data,
+                label_names=train_labels,
+                centers=train_centers,
+                batch_size=batch_size,
+                size=patch_size,
+                nlabels=4,
+                dfactor=dfactor,
+                preload=preload,
+                split=True,
+                iseg=True,
+                experimental=experimental,
+                datatype=np.float32
+            ),
+            validation_data=load_patch_batch_train(
+                image_names=val_data,
+                label_names=val_labels,
+                centers=val_centers,
+                batch_size=batch_size,
+                size=patch_size,
+                nlabels=4,
+                dfactor=dfactor,
+                preload=preload,
+                split=True,
+                iseg=True,
+                experimental=experimental,
+                datatype=np.float32
+            ),
+            steps_per_epoch=train_steps_per_epoch,
+            validation_steps=val_steps_per_epoch,
+            max_q_size=queue,
+            epochs=epochs
+        )
+        net.save(net_name)
+    return net, sufix
+
+
+def test_net(net, p, gt_name, options, sufix):
+    # Testing hyperparameters
+    patch_width = options['patch_width']
+    patch_size = (patch_width, patch_width, patch_width)
+    batch_size = options['batch_size']
+    # Data loading parameters
+    preload = options['preload']
+    queue = options['queue']
+
+    c = color_codes()
+    p_name = '-'.join(p[0].rsplit('/')[-1].rsplit('.')[0].rsplit('-')[:-1])
+    patient_path = '/'.join(p[0].rsplit('/')[:-1])
+    outputname = os.path.join(patient_path, p_name + sufix + 'brain.roi.hdr')
+    gt_nii = load_nii(gt_name)
+    gt = np.copy(np.squeeze(gt_nii.get_data()))
+    vals = np.unique(gt.flatten())
+    try:
+        image = np.squeeze(load_nii(outputname).get_data())
+    except IOError:
+        roi = np.squeeze(load_nii(p[0]).get_data())
+        centers = get_mask_voxels(roi.astype(dtype=np.bool))
+        test_samples = np.count_nonzero(roi)
+        image = np.zeros_like(roi).astype(dtype=np.uint8)
+        print(c['c'] + '[' + strftime("%H:%M:%S") + ']    ' + c['g'] +
+              '<Creating the probability map ' + c['b'] + p_name + c['nc'] + c['g'] +
+              ' (%d samples)>' % test_samples + c['nc'])
+        test_steps_per_epoch = -(-test_samples / batch_size)
+        y_pr_pred = net.predict_generator(
+            generator=load_patch_batch_generator_test(
+                image_names=p,
+                centers=centers,
+                batch_size=batch_size,
+                size=patch_size,
+                preload=preload,
+            ),
+            steps=test_steps_per_epoch,
+            max_q_size=queue
+        )
+        [x, y, z] = np.stack(centers, axis=1)
+
+        for num, results in enumerate(y_pr_pred):
+            brain = np.argmax(results, axis=1)
+            image[x, y, z] = brain
+            if num is 0:
+                im = sufix + 'csf.'
+                gt_nii.get_data()[:] = np.expand_dims(image, axis=3)
+            elif num is 1:
+                im = sufix + 'gm.'
+                gt_nii.get_data()[:] = np.expand_dims(image, axis=3)
+            elif num is 2:
+                im = sufix + 'wm.'
+                gt_nii.get_data()[:] = np.expand_dims(image, axis=3)
+            elif num is 3:
+                im = sufix + 'brain.'
+                gt_nii.get_data()[:] = np.expand_dims(vals[image], axis=3)
+            elif num is 4:
+                im = sufix + 'rf.'
+                gt_nii.get_data()[:] = np.expand_dims(vals[image], axis=3)
+            else:
+                im = sufix + 'merge.'
+                gt_nii.get_data()[:] = np.expand_dims(vals[image], axis=3)
+            roiname = os.path.join(patient_path, 'deep-' + p_name + sufix + im + 'roi.img')
+            print(c['g'] + '                   -- Saving image ' + c['b'] + roiname + c['nc'])
+            save_nii(gt_nii, roiname)
+
+        y_pred = np.argmax(y_pr_pred[-1], axis=1)
+
+        image[x, y, z] = y_pred
+
+    return image, gt
+
+
+def main():
+    options = parse_inputs()
+    c = color_codes()
+
+    experimental = options['experimental']
+
     exp_s = c['b'] + '(experimental)' if experimental else c['b'] + '(baseline)'
     print(c['c'] + '[' + strftime("%H:%M:%S") + '] ' + 'Starting cross-validation ' + exp_s + c['nc'])
     # N-fold cross validation main loop (we'll do 2 training iterations with testing for each patient)
@@ -214,146 +244,13 @@ def main():
               % (len(train_data), len(train_labels), len(val_data), len(val_labels), len(test_data)) + c['nc'])
         # Prepare the data relevant to the leave-one-out (subtract the patient from the dataset and set the path)
         # Also, prepare the network
-        net_name = os.path.join(path, 'iseg2017.fold%d' % i + sufix + 'mdl')
-
-        # First we check that we did not train for that patient, in order to save time
-        try:
-            net = keras.models.load_model(net_name)
-        except IOError:
-            # NET definition using Keras
-            train_centers = get_cnn_centers(train_data[:, 0], train_labels)
-            val_centers = get_cnn_centers(val_data[:, 0], val_labels)
-            train_samples = len(train_centers)/dfactor
-            val_samples = len(val_centers) / dfactor
-            print(c['c'] + '[' + strftime("%H:%M:%S") + ']    ' + c['g'] + 'Creating and compiling the model ' +
-                  c['b'] + '(%d samples)' % train_samples + c['nc'])
-            train_steps_per_epoch = -(-train_samples/batch_size)
-            val_steps_per_epoch = -(-val_samples / batch_size)
-            input_shape = (2,) + patch_size
-            # This architecture is based on the functional Keras API to introduce 3 output paths:
-            # - Whole tumor segmentation
-            # - Core segmentation (including whole tumor)
-            # - Whole segmentation (tumor, core and enhancing parts)
-            # The idea is to let the network work on the three parts to improve the multiclass segmentation.
-            merged_inputs = Input(shape=input_shape, name='merged_inputs')
-
-            network_func = [get_network_1, get_network_2, get_network_3]
-            weights, outputs = network_func[experimental](
-                merged_inputs,
-                filters_list,
-                kernel_size_list,
-                dense_size
-            )
-
-            net = Model(inputs=merged_inputs, outputs=outputs)
-
-            net.compile(
-                optimizer='adadelta',
-                loss='categorical_crossentropy',
-                loss_weights=weights,
-                metrics=['accuracy']
-            )
-
-            print(c['c'] + '[' + strftime("%H:%M:%S") + ']    ' +
-                  c['g'] + 'Training the model with a generator for ' +
-                  c['b'] + '(%d parameters)' % net.count_params() + c['nc'])
-            print(net.summary())
-            net.fit_generator(
-                generator=load_patch_batch_train(
-                    image_names=train_data,
-                    label_names=train_labels,
-                    centers=train_centers,
-                    batch_size=batch_size,
-                    size=patch_size,
-                    nlabels=4,
-                    dfactor=dfactor,
-                    preload=preload,
-                    split=True,
-                    iseg=True,
-                    experimental=experimental,
-                    datatype=np.float32
-                ),
-                validation_data=load_patch_batch_train(
-                    image_names=val_data,
-                    label_names=val_labels,
-                    centers=val_centers,
-                    batch_size=batch_size,
-                    size=patch_size,
-                    nlabels=4,
-                    dfactor=dfactor,
-                    preload=preload,
-                    split=True,
-                    iseg=True,
-                    experimental=experimental,
-                    datatype=np.float32
-                ),
-                steps_per_epoch=train_steps_per_epoch,
-                validation_steps=val_steps_per_epoch,
-                max_q_size=queue,
-                epochs=epochs
-            )
-            net.save(net_name)
+        net, sufix = train_net(i, train_data, train_labels, val_data, val_labels, options)
 
         # Then we test the net.
         for p, gt_name in zip(test_data, test_labels):
+            image, gt = test_net(net, p, gt_name, options, sufix)
             p_name = '-'.join(p[0].rsplit('/')[-1].rsplit('.')[0].rsplit('-')[:-1])
-            patient_path = '/'.join(p[0].rsplit('/')[:-1])
-            outputname = os.path.join(patient_path, p_name + sufix + 'brain.roi.hdr')
-            gt_nii = load_nii(gt_name)
-            gt = np.copy(np.squeeze(gt_nii.get_data()))
             vals = np.unique(gt.flatten())
-            try:
-                image = np.squeeze(load_nii(outputname).get_data())
-            except IOError:
-                roi = np.squeeze(load_nii(p[0]).get_data())
-                centers = get_mask_voxels(roi.astype(dtype=np.bool))
-                test_samples = np.count_nonzero(roi)
-                image = np.zeros_like(roi).astype(dtype=np.uint8)
-                print(c['c'] + '[' + strftime("%H:%M:%S") + ']    ' + c['g'] +
-                      '<Creating the probability map ' + c['b'] + p_name + c['nc'] + c['g'] +
-                      ' (%d samples)>' % test_samples + c['nc'])
-                test_steps_per_epoch = -(-test_samples / batch_size)
-                y_pr_pred = net.predict_generator(
-                    generator=load_patch_batch_generator_test(
-                        image_names=p,
-                        centers=centers,
-                        batch_size=batch_size,
-                        size=patch_size,
-                        preload=preload,
-                    ),
-                    steps=test_steps_per_epoch,
-                    max_q_size=queue
-                )
-                [x, y, z] = np.stack(centers, axis=1)
-
-                for num, results in enumerate(y_pr_pred):
-                    brain = np.argmax(results, axis=1)
-                    image[x, y, z] = brain
-                    if num is 0:
-                        im = sufix + 'csf.'
-                        gt_nii.get_data()[:] = np.expand_dims(image, axis=3)
-                    elif num is 1:
-                        im = sufix + 'gm.'
-                        gt_nii.get_data()[:] = np.expand_dims(image, axis=3)
-                    elif num is 2:
-                        im = sufix + 'wm.'
-                        gt_nii.get_data()[:] = np.expand_dims(image, axis=3)
-                    elif num is 3:
-                        im = sufix + 'brain.'
-                        gt_nii.get_data()[:] = np.expand_dims(vals[image], axis=3)
-                    elif num is 4:
-                        im = sufix + 'rf.'
-                        gt_nii.get_data()[:] = np.expand_dims(vals[image], axis=3)
-                    else:
-                        im = sufix + 'merge.'
-                        gt_nii.get_data()[:] = np.expand_dims(vals[image], axis=3)
-                    roiname = os.path.join(patient_path, 'deep-' + p_name + sufix + im + 'roi.img')
-                    print(c['g'] + '                   -- Saving image ' + c['b'] + roiname + c['nc'])
-                    save_nii(gt_nii, roiname)
-
-                y_pred = np.argmax(y_pr_pred[-1], axis=1)
-
-                image[x, y, z] = y_pred
 
             gt_mask = np.sum(
                 map(lambda (l, val): np.array(gt == val, dtype=np.uint8) * l, enumerate(vals)), axis=0
