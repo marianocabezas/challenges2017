15575e995ded98011aea7c5061d78617565eadda
Mariano Cabezas <mcabezas@eia.udg.edu>
2017-07-20 11:18:19 +0200

    - iSeg stuff - BRATS stuff

--- train_test_iseg.py
+++ train_test_iseg.py
@@ -247,6 +247,7 @@ def main():
             dsc_results.append(results)
             print('Subject %s DSC: %f/%f/%f' % results)
 
+    print(np.asarray(dsc_results))
     f_dsc = tuple(np.asarray(dsc_results).mean(axis=0))
     print('Final results DSC: %f/%f/%f' % f_dsc)
 
