3e7ee357bcccfea1745cd12830225b7cdd691a11
Mariano Cabezas <mcabezas@eia.udg.edu>
2017-07-31 09:39:33 +0200

    - Adapted cnn nolearn code to the method of the paper

--- nets.py
+++ nets.py
@@ -239,13 +239,12 @@ def get_iseg_experimental4(input_shape, filters_list, kernel_size_list, dense_si
     full = PReLU()(full)
     full = Conv3D(4, kernel_size=(1, 1, 1), data_format='channels_first')(full)
 
-    full_shape = K.int_shape(full)
     rf = full
     print(K.int_shape(rf))
 
     while np.product(K.int_shape(rf)[1:]) > 1:
         rf = Conv3D(4, kernel_size=(3, 3, 3), data_format='channels_first')(rf)
-        print(K.int_shape(rf))
+        print(K.int_shape(rf), np.product(K.int_shape(rf)[1:]))
     rf = Flatten()(rf)
 
     # FC labeling
