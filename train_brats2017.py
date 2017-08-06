888e0ab8cf45b14a58a52ba3b8ef406d4e2c3cf7
marianocabezas <mcabezas@silver.udg.edu>
2017-08-06 15:00:34 +0200

    - Brats stuff

--- train_brats2017.py
+++ train_brats2017.py
@@ -17,7 +17,7 @@ def parse_inputs():
     parser.add_argument('-i', '--patch-width', dest='patch_width', type=int, default=17)
     parser.add_argument('-k', '--kernel-size', dest='conv_width', nargs='+', type=int, default=3)
     parser.add_argument('-c', '--conv-blocks', dest='conv_blocks', type=int, default=5)
-    parser.add_argument('-b', '--batch-size', dest='batch_size', type=int, default=2048)
+    parser.add_argument('-b', '--batch-size', dest='batch_size', type=int, default=1024)
     parser.add_argument('-d', '--dense-size', dest='dense_size', type=int, default=256)
     parser.add_argument('-D', '--down-factor', dest='dfactor', type=int, default=50)
     parser.add_argument('-n', '--num-filters', action='store', dest='n_filters', nargs='+', type=int, default=[32])
