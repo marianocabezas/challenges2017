deeb0c9b7141ab03605c1fa6322e4c50957dcff1
Mariano Cabezas <mcabezas@eia.udg.edu>
2017-12-20 13:06:51 +0100

    ?

--- layers.py
+++ layers.py
@@ -352,6 +352,8 @@ class CapsuleLayer(layers.Layer):
 
     def build(self, input_shape):
         assert len(input_shape) >= 3, "The input Tensor should have shape=[None, input_num_capsule, input_dim_vector]"
+        print(input_shape[1])
+        print(input_shape[2])
         self.input_num_capsule = input_shape[1]
         self.input_dim_vector = input_shape[2]
 
