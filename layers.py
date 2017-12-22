658fb8c53d835a0239f09b496848f86df8927ffd
Mariano Cabezas <mcabezas@eia.udg.edu>
2017-12-22 10:15:04 +0100

    ?

--- layers.py
+++ layers.py
@@ -401,8 +401,14 @@ class CapsuleLayer(layers.Layer):
         outputs = None
         for i in range(self.num_routing):
             permuted_bias = K.permute_dimensions(self.bias, (0, 1, 4, 3, 2))
-            bias_shape = K.int_shape(permuted_bias)
-            soft_bias = K.reshape(K.softmax(K.reshape(permuted_bias, (-1, bias_shape[-1]))), bias_shape)
+            soft_bias = K.reshape(
+                K.softmax(
+                    K.reshape(
+                        permuted_bias,
+                        (-1, self.num_capsule))
+                ),
+                (1, self.input_num_capsule, 1, 1, self.num_capsule)
+            )
             c = K.permute_dimensions(soft_bias, (0, 1, 4, 3, 2))
             # c = K.permute_dimensions(
             #     K.softmax(K.permute_dimensions(self.bias, (0, 1, 4, 3, 2))),
